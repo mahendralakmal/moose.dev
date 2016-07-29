fd2fec1be83c59a517923cb811e4f5e01f5a0e88
Mahendra <mahen@Mahendras-MacBook-Pro.local>
2016-07-29 12:59:04 +0530

    Update navigations

--- nav.php
+++ nav.php
@@ -3,11 +3,11 @@
     <a id="logo-container" href="/" class="brand-logo"><img src="/images/Moose_CC_Logo_23May2016.png" alt="Logo"></a>
 <!--    <a id="logo-container" href="./index.php" class="brand-logo"><img src="./images/Moose_CC_Logo_23May2016.png" width="100" height="75" alt="Logo"></a>-->
     <ul class="right hide-on-med-and-down">
-      <li><a href="/">Home</a></li>
+      <li class="nav-home"><a href="/">Home</a></li>
       <li class="nav-women"><a href="/women/product-grid.php">Women</a></li>
       <li class="nav-men"><a href="/men/product-grid.php">Men</a></li>
-      <li><a href="/about.php">About Us</a></li>
-      <li><a href="/contact.php">Contact Us</a></li>
+      <li class="nav-about"><a href="/about.php">About Us</a></li>
+      <li class="nav-contact"><a href="/contact.php">Contact Us</a></li>
       <li class="nav-cart cart"><a href="./cart.php"><i class="material-icons">shopping_cart</i></a></li>
     </ul>
 
